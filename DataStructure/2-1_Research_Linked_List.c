c3215c9feec4f69680a92637673e41f05f6e489a
COCOBLUE <cocoblue@kakao.com>
2020-08-14 15:30:23 +0900

    Add 2-1

--- DataStructure/2-1_Research_Linked_List.c
+++ DataStructure/2-1_Research_Linked_List.c
@@ -1,30 +1,30 @@
-// 출처: 한빛아카데미 'C로 배우는 쉬운 자료구조'
+// 일부 출처: 한빛아카데미 'C로 배우는 쉬운 자료구조'
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 // 단순 연결 리스트의 노드 구조를 구조체로 정의
-typedef struct ListNode {
+typedef struct ListNode1 {
     // 데이터가 들어갈 공간
     char data[100];
     // head가 들어갈 구조체
-    struct ListNode* link;
+    struct ListNode1* link;
 // 구조체 별칭을 listNode로 선언
-} listNode;
+} listNode1;
 
 // 리스트 시작을 나타내는 head 노드를 구조체로 정의
 typedef struct {
     // listNode의 메모리 주소를 가르킬 수 있는 head
-    listNode* head;
+    listNode1* head;
 // 구조체 별칭을 linkedList_h로 선언
-} linkedList_h;
+} linkedList_h1;
 
 // 공백 연결 리스트를 생성하는 함수
-linkedList_h* createLinkedList_h(void) {
+linkedList_h1* createLinkedList_h1(void) {
     // linkedList_h 포인터 L 선언
-    linkedList_h* L;
+    linkedList_h1* L;
     // linkedList_h의 사이즈 만큼의 메모리를 L에 할당
-    L = (linkedList_h*)malloc(sizeof(linkedList_h));
+    L = (linkedList_h1*)malloc(sizeof(linkedList_h1));
     // 다음 링크의 포인터는 NULL로 정의
     L -> head = NULL;
     // L을 리턴. -> data 부분에 데이터를 넣지 않았고, head를 NULL로 지정했으므로, 공백 연결 리스트를 만든 것이 됨.
@@ -32,9 +32,9 @@ linkedList_h* createLinkedList_h(void) {
 }
 
 // 연결 리스트의 전체 메모리를 해제하는 연산
-void freeLinkedList_h(linkedList_h* L) {
+void freeLinkedList_h1(linkedList_h1* L) {
     // 임시적으로 저장할 변수 정의
-    listNode* p;
+    listNode1* p;
     // 이어지는 link가 끝이 날때까지 반복
     while(L -> head != NULL) {
         // 임시로 정의한 p에 head 포인터를 가르킴
@@ -49,9 +49,9 @@ void freeLinkedList_h(linkedList_h* L) {
 }
 
 // 연결 리스트를 순서대로 출력하는 연산
-void printList(linkedList_h* L) {
+void printList1(linkedList_h1* L) {
     // 임시 구조체 포인터 p
-    listNode* p;
+    listNode1* p;
     // 출력 시작
     printf("L = (");
     // p를 입력 받은 노드의 다음 노드를 가르키게 만듬.
@@ -75,11 +75,11 @@ void printList(linkedList_h* L) {
 }
 
 // 첫 번째 노드로 삽입하는 연산
-void insertFirstNode(linkedList_h *L, char *x) {
+void insertFirstNode1(linkedList_h1 *L, char *x) {
     // 새로운 노드의 데이터를 저장할 포인터
-    listNode* newNode;
+    listNode1* newNode;
     // 새로운 노드에 메모리 할당
-    newNode = (listNode*)malloc(sizeof(listNode));
+    newNode = (listNode1*)malloc(sizeof(listNode1));
     // 새로운 노드의 데이터 부분에 입력 받은 데이터를 넣음.
     strcpy(newNode -> data, x);
     // 첫번째 노드에 삽입하는 것이므로 다음 노드의 링크를 현재의 첫번째 노드로 함.
@@ -89,13 +89,13 @@ void insertFirstNode(linkedList_h *L, char *x) {
 }
 
 // 마지막 노드로 삽입하는 연산
-void insertLastNode(linkedList_h* L, char* x) {
+void insertLastNode1(linkedList_h1* L, char* x) {
     // 새로운 노드를 저장할 포인터
-    listNode* newNode;
+    listNode1* newNode;
     // 임시적으로 저장할 포인터
-    listNode* temp;
+    listNode1* temp;
     // 새로운 노드의 메모리를 할당함
-    newNode = (listNode*)malloc(sizeof(listNode));
+    newNode = (listNode1*)malloc(sizeof(listNode1));
     // 새로운 노드의 데이터 부분에 입력받은 데이터를 넣음.
     strcpy(newNode -> data, x);
     // 마지막에 넣는 것이니 link 부분은 NULL로 설정
@@ -116,27 +116,37 @@ void insertLastNode(linkedList_h* L, char* x) {
     
 }
 
-int singly_linked_list() {
-    linkedList_h* L;
-    L = createLinkedList_h();
-    printf("공백 리스트 생성\n");
-    printList(L);
+
+// 다음 노드의 주소를 표시하는 함수
+void printNextAddress(linkedList_h1* L) {
+    // 임시적으로 저장할 포인터
+    listNode1* temp;
+    // 첫번째 노드를 temp에 저장
+    temp = L -> head;
     
-    printf("리스트에 [수] 노드 삽입\n");
-    insertFirstNode(L, "수");
-    printList(L);
+    // 다음 노드가 없을 때까지 반복
+    while(temp -> link != NULL) {
+        // 다음 노드의 주소 출력
+        printf("현재 노드의 주소: 0x%p\n", temp);
+        printf("다음 노드의 주소: 0x%p\n", temp -> link);
+        // temp의 데이터를 다음 노드로 변경
+        temp = temp -> link;
+    }
     
-    printf("리스트 마지막에 [금] 노드 삽입\n");
-    insertLastNode(L, "금");
-    printList(L);
-    
-    printf("리스트 첫번째에 [월] 노드 삽입\n");
-    insertFirstNode(L, "월");
-    printList(L);
-    
-    printf("리스트 공간을 해제하여 공백 리스트로 만들기\n");
-    freeLinkedList_h(L);
-    printList(L);
+    printf("현재 노드의 주소: 0x%p\n", temp);
+
+}
+
+// 메인 함수
+int research_linked_list() {
+    linkedList_h1* L;
+    L = createLinkedList_h1();
+    insertFirstNode1(L, "수");
+    insertLastNode1(L, "금");
+    insertFirstNode1(L, "월");
+    printList1(L);
+    printf("첫번째 노드 주소: 0x%p\n", L -> head);
+    printNextAddress(L);
     
     return 0;
 }
